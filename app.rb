6e086d1df5984a427c218b1c9d169b3007a59dfc
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-20 22:59:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,18 +1,13 @@
+require_relative 'config/environment'
 
 class App < Sinatra::Base
+
 	get '/' do
 		erb :index
 	end
 
-	get "/hello" do
-		erb :hello
+	get '/info' do
+		erb :info
 	end
 
-	get "/goodbye" do
-		erb :goodbye
-	end
-
-	get "/date" do
-		erb :date
-	end
 end
