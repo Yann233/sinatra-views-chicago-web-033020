1abe41cfa5e803fc89845a2b01ca024d7c15aab6
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-20 22:45:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,14 +1,20 @@
+
 class App < Sinatra::Base
 
-	get '/hello' do
+	get '/' do
+		erb :index
+	end
+
+	get "/hello" do
 		erb :hello
 	end
 
-	get '/date' do
-		erb :date
-	end
-
-	get '/goodbye' do
+	get "/goodbye" do
 		erb :goodbye
 	end
+
+	get "/date" do
+		erb :date
+	end
+	
 end
